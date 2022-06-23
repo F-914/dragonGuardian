23fac92e5cf88747c57a81bc465394be590a2b05
xingzhou010321 <65446674+xingzhou010321@users.noreply.github.com>
2022-06-23 16:33:59 +0800

    Add files via upload
    大厅战斗界面，不包括信息栏和下边栏

---
+++ src/app/scenes/MainScene.lua
@@ -0,0 +1,34 @@
+--[[--
+    该类仅用于测试
+    MainScene.lua
+]]
+local MainScene = class("MainScene", function()
+    return display.newScene("MainScene")
+end)
+
+local GameData = require("src/app/data/GameData.lua")
+local MainUIBattle = require("src/app/ui/MainUIBattleView.lua")
+
+function MainScene:ctor()
+    GameData:init()
+
+    self.mainUIBattle_ = MainUIBattle.new()
+    self:addChild(self.mainUIBattle_)
+
+    self:addNodeEventListener(cc.NODE_ENTER_FRAME_EVENT, handler(self, self.update))
+    self:performWithDelay(function()
+        self:scheduleUpdate()
+    end, 1)
+
+end
+function MainScene:update(dt)
+    GameData:update(dt)
+    self.mainUIBattle_:update(dt)
+end
+function MainScene:onEnter()
+end
+
+function MainScene:onExit()
+end
+
+return MainScene
