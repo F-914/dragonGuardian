db93b02120bc873f43d48b556597d9b9d88ff4c4
Zoybzo <lr2561914955@outlook.com>
2022-07-01 13:37:24 +0800

    更新的文件

--- src/app/def/EventDef.lua
+++ src/app/def/EventDef.lua
@@ -4,6 +4,11 @@
 ]]
 local EventDef = {}
 
-EventDef.ID = {}
+EventDef.ID = {
+    DESTORY_POPUP=1,--销毁使用弹窗(特定图鉴二级面板)
+    SHOW_BAG=2,--显示图鉴界面中的图鉴
+    HIDE_BAG=3,--隐藏图鉴界面中的图鉴
+    RESUME_BAG_BUTTON=4--唤醒图鉴界面中的按钮(图标)
+}
 
 return EventDef
