40d5ad04cb595ea76a8371701130464314021816
Zoybzo <lr2561914955@outlook.com>
2022-07-08 12:01:13 +0800

    更新了Skill

--- src/app/data/Skill.lua
+++ src/app/data/Skill.lua
@@ -13,11 +13,7 @@ local EventManager = require("app.manager.EventManager")
 
 ---Skill.ctor 构造函数
 ---@param skillName         string 技能名
----@param skillVariable     string 影响的变量
----@param skillValue        number 影响的值
----@param skillValueUpgrade number 升级后技能数值的变化
----@param skillValueEnhance number 强化后技能数值的变化
----@return  Type Description
+---@param skillFun          function 技能函数
 function Skill:ctor(skillName, skillFun)
     self:setSkill(skillName, skillFun)
     EventManager:doEvent(EventDef.ID.CREATE_SKILL, self)
@@ -25,15 +21,19 @@ end
 
 function Skill:setSkill(skillName, skillFun)
     self.skillName_ = skillName
-    self:skillFun() = skillFun
+    self.skillFun_ = skillFun
+end
+
+function Skill:setSkillFun(skillFun)
+    self.skillFun_ = skillFun
+end
+
+function Skill:useSkillFun(...)
+    return self.skillFun_(...)
 end
 
 function Skill:getSkillName()
     return self.skillName_
-end
-
-function Skill:skillFun()
-
 end
 
 return Skill
