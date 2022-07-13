bc09a21e58296cd1366986b13315eec3cc264a8f
xingzhou010321 <65446674+xingzhou010321@users.noreply.github.com>
2022-07-13 13:19:45 +0800

    Add files via upload
    适应了outGameData中的数据结构

--- src/app/ui/secondaryui/OpenTreasure2nd.lua
+++ src/app/ui/secondaryui/OpenTreasure2nd.lua
@@ -10,16 +10,17 @@ local Factory = require("app.utils.Factory")
 local ChestRewardGet2nd = require("app.ui.secondaryui.ChestRewardGet2nd")
 local TestDataFactory = require("app.test.TestDataFactory")
 local StringDef = require("app.def.StringDef")
+local OutGameData = require("src/app/data/OutGameData.lua")
 --[[--
     @description: 构造函数
     @param treasureData type:table, 宝箱中的数据
     @return none
 ]]
-function OpenTreasureChest2nd:ctor(treasureData, node)
+function OpenTreasureChest2nd:ctor(treasureBox, node)
 
-    self.treasureData_ = treasureData --这个treasureData并不是宝箱的奖励,而
-                                      --是宝箱可能的奖励,仅用于显示
-    self.node_ = node
+    self.data_ = treasureBox --表示奖励宝箱
+    ---这个没啥用，注释掉
+    --self.node_ = node
     self:init()
 end
 
@@ -42,12 +43,12 @@ function OpenTreasureChest2nd:init()
     backSprite:setScale(.9)
     local size = backSprite:getContentSize()
 
-    local chestSprite = Factory:createRewardSprite(self.treasureData_.name)
+    local chestSprite = Factory:createRewardSprite(self.data_.type)
     chestSprite:setPosition(size.width * .5, size.height * 1.2)
     chestSprite:setScale(.9)
     chestSprite:addTo(backSprite)
 
-    local fontSprite = Factory:createChestFontSprite(self.treasureData_.name)
+    local fontSprite = Factory:createChestFontSprite(self.data_.type)
     fontSprite:setPosition(size.width * .5, size.height * .9)
     fontSprite:addTo(backSprite)
 
@@ -65,22 +66,18 @@ function OpenTreasureChest2nd:init()
     openButton:addTouchEventListener(function(sender, eventType)
         if eventType == 2 then
             --[[--
-                这里用于调用outGameData的打开宝箱的函数
-                这个函数需要生成随机奖励,并将数据上传服务器,并同步
-                最后返回宝箱的奖励信息
-                下面暂时用假数据替代
+                这里应该调用outGameData的随机生成奖励的函数，用于生成奖励
+                同时想服务器发送消息，然后将数据传输给另一个显示宝箱开出的奖励的而日记界面
             ]]
-            --self:playOpenChestAnimation()
             local newView = ChestRewardGet2nd.new(TestDataFactory:getOpenChestItemData())
-            self.node_:get()
-
             newView:addTo(self:getParent())
             self:removeSelf()
         end
     end)
     openButton:addTo(backSprite)
     --接下来部分暂时没有数据，所以随便填写数据
-    local chestInforPane = Factory:createChestRewardPane(self.treasureData_)
+    local chestInforPane = Factory:createChestRewardPane(OutGameData
+            :getTreasureBoxRewardWinningRate()[self.data_.type])
     chestInforPane:setPosition(display.width * .5, display.height * .5)
     chestInforPane:addTo(self)
 
