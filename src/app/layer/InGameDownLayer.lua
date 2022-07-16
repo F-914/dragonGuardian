1d671227524b19aab79972fd7f944dd889b98b37
xingzhou010321 <65446674+xingzhou010321@users.noreply.github.com>
2022-07-16 17:01:01 +0800

    Add files via upload
    我的这一部分的所有报错都已经解决了吧（大概）

---
+++ src/app/layer/InGameDownLayer.lua
@@ -0,0 +1,171 @@
+--[[--
+    游戏内第二层，包括底部背景及敌人和方格内塔
+    InGameDownLayer.lua
+]]
+local InGameDownLayer = class("InGameDownLayer", function()
+    return display.newLayer()
+end)
+
+--local
+local ConstDef = require("app.def.ConstDef")
+local InGameData = require("app.data.InGameData")
+local EventDef = require("app.def.EventDef")
+local EventManager = require("app.manager.EventManager")
+local InGameEnemySprite = require("app.ui.node.InGameEnemySprite")
+local X_LEFT = 0
+local X_RIGHT = display.width*17/20 - display.cx/22
+local Y_DOWN_PLAYER = 0
+local Y_UP_PLAYER = display.cy*14/20
+local schedule = cc.Director:getInstance():getScheduler()	--计时器路径
+local timeCreateEnemySchdule = nil  -- 敌人生成计时器
+local littleEnemyNum = 5    -- 每五只小怪一只精英怪
+--
+
+function InGameDownLayer:ctor()
+    --self.bulletMap_ = {} -- 类型：table，Key：bullet， Value：bulletSprite 子弹
+    self.enemyMap_ = {}
+
+    self:setContentSize(display.width, display.height)
+    self:init()
+    self:onEnter()
+end
+
+--[[--
+    节点进入
+
+    @param none
+
+    @return none
+]]
+function InGameDownLayer:onEnter()
+    EventManager:regListener(EventDef.ID.CREATE_ENEMY, self, function(enemy)
+        --修改：可变换的敌人类型(用数字table存储种类顺序？)
+        local enemyNode = InGameEnemySprite.new(1, enemy)
+        self:addChild(enemyNode)
+        self.enemyMap_[enemy] = enemyNode
+    end)
+
+    EventManager:regListener(EventDef.ID.DESTORY_ENEMY, self, function(enemy)
+        local node = self.enemyMap_[enemy]
+        node:removeFromParent()
+        self.enemyMap_[enemy] = nil
+    end)
+
+end
+
+--[[--
+    节点退出
+
+    @param none
+
+    @return none
+]]
+function InGameDownLayer:onExit()
+    EventManager:unRegListener(EventDef.ID.CREATE_ENEMY, self)
+    EventManager:unRegListener(EventDef.ID.DESTORY_ENEMY, self)
+end
+
+
+function InGameDownLayer:init()
+    local basemapDown = cc.Sprite:create("battle_in_game/battle_view/basemap_down.png")
+    basemapDown:setAnchorPoint(0.5, 0.5)
+    basemapDown:setPosition(display.cx, display.cy)
+    basemapDown:addTo(self)
+    --self:createEnemy()
+
+    local enemies = InGameData:getEnemies()
+    for i = 1, #enemies do
+        local enemy = InGameEnemySprite.new(1, enemies[i])
+        self:addChild(enemy)
+        self.enemyMap_[enemies[i]] = enemy
+    end
+end
+
+--[[--
+    描述：计时器连续生成敌人
+]]
+function InGameDownLayer:createEnemy()
+    -- 连续生成敌人
+    local num = 0
+    timeCreateEnemySchdule = schedule:scheduleScriptFunc(function (dt)
+        if InGameData:getGameState() == ConstDef.GAME_STATE.PLAY then
+            if num < littleEnemyNum then
+                num = num + 1
+                local enemy = InGameEnemySprite.new(1)
+                self:addChild(enemy)
+                self:enemyMovePlayer(enemy)
+            elseif num == littleEnemyNum then
+                num = 0
+                local enemy = InGameEnemySprite.new(2)
+                self:addChild(enemy)
+                self:enemyMovePlayer(enemy)
+            end
+        end
+    end, 0.5, false)
+end
+
+--[[--
+    描述：停止敌人生成计时器
+]]
+function InGameDownLayer:stopCreateEnemy()
+    schedule:unscheduleScriptEntry(timeCreateEnemySchdule)
+end
+
+--[[--
+	描述：敌人运动路径(我方)
+]]
+function InGameDownLayer:enemyMovePlayer(enemy)
+	local timeSchedule = nil
+	local step = 1	--运动步骤（1-3）
+    local curX, curY    --敌人当前位置
+    local deltaX = 10
+    local deltaY = 10
+
+	timeSchedule = schedule:scheduleScriptFunc(function (dt)
+        if InGameData:getGameState() == ConstDef.GAME_STATE.PLAY then
+            curX = enemy:getPositionX()
+            curY = enemy:getPositionY()
+
+            if step == 1 then
+                -- 左下到左上
+                if curY + deltaY < Y_UP_PLAYER then
+                    enemy:setPosition(curX, curY + deltaY)
+                else
+                    enemy:setPosition(curX, Y_UP_PLAYER)
+                    step = 2
+                end
+            elseif step == 2 then
+                -- 左上到右上
+                if curX + deltaX < X_RIGHT then
+                    enemy:setPosition(curX + deltaX, curY)
+                else
+                    enemy:setPosition(X_RIGHT, curY)
+                    step = 3
+                end
+            elseif step == 3 then
+                -- 右上到右下
+                if curY > Y_DOWN_PLAYER then
+                    enemy:setPosition(curX, curY - deltaY)
+                else
+                    schedule:unscheduleScriptEntry(timeSchedule)
+                    enemy:removeFromParent()
+                end
+            end
+		end
+	end, 0.05, false)
+end
+
+--[[--
+    帧刷新
+
+    @param dt 类型：number，帧间隔，单位秒
+
+    @return none
+]]
+function InGameDownLayer:update(dt)
+    for _, node in pairs(self.enemyMap_) do
+        node:update(dt)
+    end
+end
+
+return InGameDownLayer
---
+++ src/app/node/InGameTowerButton.lua
@@ -0,0 +1,193 @@
+--[[--
+    InGameTowerButton.lua
+
+    描述：游戏内生成在格子中的塔图标按钮，每个塔包含右上角的等级标识
+]]
+--[[--
+    游戏内塔按键
+    InGameTowerButton
+]]
+local InGameTowerButton = class("InGameTowerButton", function(res)
+    return display.newSprite()
+end)
+
+--local
+local TowerArrayDef = require("app.def.TowerArrayDef")
+local GirdLocation = require("app.def.GirdLocation")
+local Log = require("app.utils.Log")
+local towerTypeSprite
+local sizeTowerButton
+
+function InGameTowerButton:ctor(type)   -- 塔属于我方type=1，还是对方type=2
+    if type == 1 then
+        self.numPlayer_ = math.random(1, 5)   -- 生成第几个塔
+        self:buildPlayerTower(self.numPlayer_)
+        self:touchMove()    -- 敌方塔不可移动
+    elseif type == 2 then
+        self.numEnemy_ = math.random(1, 5)
+        self:buildEnemyTower(self.numEnemy_)
+    else
+        Log.w("The parameter range is 1(player) or 2(enemy)")
+    end
+end
+
+--[[--
+    描述：初始化创建塔
+]]
+function InGameTowerButton:buildPlayerTower(num)
+    local towerButton = ccui.Button:create(TowerArrayDef[num].ICON_PATH)
+    towerButton:setAnchorPoint(0, 0)
+    towerButton:setPosition(0, 0)
+    towerButton:scale(0.85)
+    sizeTowerButton = towerButton:getContentSize()
+    towerButton:addTouchEventListener(function(sender, eventType)
+        if 2 == eventType then
+        end
+    end)
+    towerButton:setSwallowTouches(false)    --不吞噬下层触摸事件
+
+    self:setAnchorPoint(0.5, 0.5)
+    self:setContentSize(sizeTowerButton.width*0.85, sizeTowerButton.height*0.85)
+
+    towerTypeSprite = cc.Sprite:create("battle_in_game/battle_view/subscript_level/1.png")
+    towerTypeSprite:setAnchorPoint(1,1)
+    --towerTypeSprite:scale(0.6)
+    towerTypeSprite:setPosition(sizeTowerButton.width*0.85, sizeTowerButton.height*0.85)
+
+    --随机位置
+    local x = math.random(1, 3)
+    local y = math.random(1, 5)
+    Log.i("x,y "..x.." "..y)
+    if GirdLocation.PLAYER[x][y].IS_USED == false then
+        Log.i("build tower accept")
+        self:setPlayerLocate(towerButton, x, y)
+        GirdLocation.PLAYER[x][y].IS_USED = true
+    else
+        -- 若检测到一个已使用位置，遍历判断位置是否已满
+        local isFull = true
+        for i,v in pairs(GirdLocation.PLAYER) do
+            for j,w in pairs(v) do
+                if w.IS_USED == false then
+                    isFull = false
+                    x = i
+                    y = j
+                    Log.i("build tower accept")
+                    self:setPlayerLocate(towerButton, x, y)
+                    GirdLocation.PLAYER[x][y].IS_USED = true
+                    break
+                end
+            end
+            if isFull == false then
+                break
+            end
+        end
+        if isFull then
+            Log.i("Tower gird is full")
+        end
+    end
+end
+
+--[[--
+    描述：设定生成玩家塔位置
+]]
+function InGameTowerButton:setPlayerLocate(node, x, y)
+    self.nodeX_ = GirdLocation.PLAYER[x][y].X
+    self.nodeY_ = GirdLocation.PLAYER[x][y].Y
+    self:setPosition(self.nodeX_, self.nodeY_)
+    node:addTo(self)
+    towerTypeSprite:addTo(self)
+end
+
+--------------------------------------------------------------------------------------------
+--[[--
+    描述：初始化创建敌人塔
+]]
+function InGameTowerButton:buildEnemyTower(num)
+    local towerButton = ccui.Button:create(TowerArrayDef[num].ICON_PATH)
+    towerButton:setAnchorPoint(0, 0)
+    towerButton:setPosition(0, 0)
+    towerButton:scale(0.85)
+    sizeTowerButton = towerButton:getContentSize()
+    towerButton:addTouchEventListener(function(sender, eventType)
+        if 2 == eventType then
+        end
+    end)
+    towerButton:setSwallowTouches(false)    --不吞噬下层触摸事件
+    towerButton:setTouchEnabled(false)      --敌方塔不可交互
+
+    self:setAnchorPoint(0.5, 0.5)
+    self:setContentSize(sizeTowerButton.width*0.85, sizeTowerButton.height*0.85)
+
+    towerTypeSprite = cc.Sprite:create("battle_in_game/battle_view/subscript_level/1.png")
+    towerTypeSprite:setAnchorPoint(1,1)
+    towerTypeSprite:setPosition(sizeTowerButton.width*0.85, sizeTowerButton.height*0.85)
+
+    --随机位置
+    local x = math.random(1, 3)
+    local y = math.random(1, 5)
+    Log.i("x,y "..x.." "..y)
+    if GirdLocation.ENEMY[x][y].IS_USED == false then
+        Log.i("build tower accept")
+        self:setEnemyLocate(towerButton, x, y)
+        GirdLocation.ENEMY[x][y].IS_USED = true
+    else
+        -- 若检测到一个已使用位置，遍历判断位置是否已满
+        local isFull = true
+        for i,v in pairs(GirdLocation.ENEMY) do
+            for j,w in pairs(v) do
+                if w.IS_USED == false then
+                    isFull = false
+                    x = i
+                    y = j
+                    Log.i("build tower accept")
+                    self:setEnemyLocate(towerButton, x, y)
+                    GirdLocation.ENEMY[x][y].IS_USED = true
+                    break
+                end
+            end
+            if isFull == false then
+                break
+            end
+        end
+        if isFull then
+            Log.i("Tower gird is full")
+        end
+    end
+end
+
+--[[--
+    描述：设定生成敌人塔位置
+]]
+function InGameTowerButton:setEnemyLocate(node, x, y)
+    self.nodeX_ = GirdLocation.ENEMY[x][y].X
+    self.nodeY_ = GirdLocation.ENEMY[x][y].Y
+    self:setPosition(self.nodeX_, self.nodeY_)
+    node:addTo(self)
+    towerTypeSprite:addTo(self)
+end
+
+------------------------------------------------------------------------------------------------
+--[[--
+    描述：拖动按钮触摸事件
+]]
+function InGameTowerButton:touchMove()
+    self:addNodeEventListener(cc.NODE_TOUCH_EVENT, function(event)
+        dump(event)
+        if event.name == "began" then
+            Log.i("touch")
+            return true
+        elseif event.name == "moved" then
+            Log.i("move")
+            local curX = event.prevX
+            local curY = event.prevY
+            self:setPosition(curX, curY)
+        elseif event.name == "ended" then
+            Log.i("ended")
+            self:setPosition(self.nodeX_, self.nodeY_)
+        end
+    end)
+    self:setTouchMode(cc.TOUCH_MODE_ONE_BY_ONE)
+    self:setTouchEnabled(true)
+end
+
+return InGameTowerButton
